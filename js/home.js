136e182de17699bff2a5830e5bb72bce0d1525b5
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-06-04 20:00:48 +0300

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -6,6 +6,36 @@ select.addEventListener("input", () => {
 });
 
 let prayTime = document.querySelector(".left-time-pray");
+
 setInterval(() => {
   prayTime.innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
 }, 1000);
+
+// Api Pray Time
+let api;
+let d = new Date();
+let m = d.getMonth() - 2;
+
+api = `https://api.aladhan.com/v1/timingsByCity/${m}-${d.getDay()}-${d.getFullYear()}?city=${
+  select.value
+}&country=egypt&method=8`;
+
+let myApi = new XMLHttpRequest();
+myApi.open("GET", api);
+myApi.send();
+
+myApi.onreadystatechange = function () {
+  if (myApi.status === 200 && myApi.readyState === 4) {
+    let prayObj = JSON.parse(myApi.responseText);
+    console.log(prayObj);
+
+    let timePray = document.querySelectorAll(".time-pray p");
+    console.log(timePray);
+    timePray[0].textContent = prayObj.data.timings.Fajr + "م";
+    timePray[1].textContent = prayObj.data.timings.Sunrise + "م";
+    timePray[2].textContent = prayObj.data.timings.Dhuhr + "م";
+    timePray[3].textContent = prayObj.data.timings.Asr + "م";
+    timePray[4].textContent = prayObj.data.timings.Maghrib + "م";
+    timePray[5].textContent = prayObj.data.timings.Isha + "م";
+  }
+};
---
+++ js/read.js
@@ -0,0 +1,10 @@
+// let centerListen = document.querySelector(".center-listen");
+// let sura = document.querySelector(".sura");
+
+// centerListen.addEventListener("scroll", () => {
+//   if (centerListen.scrollTop > 0) {
+//     sura.style.marginTop = "74px";
+//   } else {
+//     sura.style.marginTop = "0";
+//   }
+// });
